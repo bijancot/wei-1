d22a23051e758a52382a6401d6c68c15f17989d2
Panji Iman Baskoro <panjidia995@gmail.com>
2020-09-10 14:09:49 +0700

    asd

--- pages/cetak1.php
+++ pages/cetak1.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />    
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
   <title>Laporan Print Denbekang V-44-02</title>
   <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css" rel="stylesheet">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
-<?php 
+<?php
 	include 'config.php';
 	if(isset($_GET['delete'])){
 		$id = $_GET['delete'];
@@ -19,16 +19,16 @@
 			$sql = "delete from tb_pinjam where id=".$id;
 		}
 	}
-	 
+
  ?>
- 
+
 <html>
 <head>
 	<title>Denbekang V-44-02</title>
-    
+
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>  
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
   <nav class="navbar navbar-dark bg-primary">
@@ -36,11 +36,11 @@
       Denbekang V-44-02 Kota Mojokerto
     </a>
   </nav>
- 
+
   <div class="container">
     <h3 align="center" class="mt-3">Laporan Denbekang V-44-02 Kota Mojokerto</h3>
 
- 
+
         <div class="panel">
 			<div class="panel-heading">
 				<h3 class="panel-title">Tabel History Barang Gudang</h3>
@@ -48,7 +48,7 @@
 			<div class="table-responsive">
 				<table class="table table-striped table-bordered">
 					<thead>
-						
+
 						<tr>
 						<th>Id</th>
 						<th>Nama Barang</th>
@@ -56,11 +56,14 @@
 						<th>Harga</th>
 						<th>Tanggal pengambilan</th>
 						<th>Tanggal Pengembalian</th></tr>
-						
+
 					</thead>
 					<tbody>
 					<?php
-						$sql = "select * from tb_pinjam a inner join tb_barang s on a.barang_id=s.barang_id ORDER BY tanggal_pengambilan ASC  ";
+            $mulai = $_GET['param1'];
+            $akhir = $_GET['param2'];
+
+						$sql = "select * from tb_pinjam a inner join tb_barang s on a.barang_id=s.barang_id where tanggal_pengambilan || tanggal_pengambilan between '$mulai' and '$akhir'  ORDER BY tanggal_pengambilan ASC  ";
 						$result = mysqli_query($koneksi, $sql);
 								if(mysqli_num_rows($result)){
 									while($row = mysqli_fetch_assoc($result)){
@@ -72,23 +75,23 @@
 						<td><?php echo $row['harga'] ?></td>
 						<td><?php echo $row['tanggal_pengambilan'] ?></td>
 						<td><?php echo $row['tanggal_pengembalian'] ?></td>
-											
+
 					</tr>
-					
+
 					<?php
 						}
 						}
 					?>
 					</tbody>
 				</table>
-				
-				
+
+
 			</div>
- 
+
         </form>
 
   </div>
- 
+
 </body>
 </html>
 <script>
